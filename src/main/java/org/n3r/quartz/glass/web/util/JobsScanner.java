09ae1e3e9ec477884cec918359f1c037c0075243
bingoohuang <bingoo.huang@gmail.com>
2014-04-23 17:13:46 +0800

    catch exception and log in JobScanner

--- src/main/java/org/n3r/quartz/glass/web/util/JobsScanner.java
+++ src/main/java/org/n3r/quartz/glass/web/util/JobsScanner.java
@@ -4,6 +4,8 @@ import com.google.common.base.Splitter;
 import org.n3r.quartz.glass.job.annotation.GlassJob;
 import org.quartz.Job;
 import org.quartz.Scheduler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
@@ -13,23 +15,29 @@ import org.springframework.core.type.filter.AssignableTypeFilter;
 import javax.annotation.PostConstruct;
 
 public class JobsScanner {
+    private Logger log = LoggerFactory.getLogger(JobsScanner.class);
     private String basePackage;
 
     @Autowired
     protected Scheduler scheduler;
 
     @PostConstruct
-    protected void scanPaths() throws Exception {
+    protected void scanPaths() {
         ClassPathScanningCandidateComponentProvider provider;
         provider = new ClassPathScanningCandidateComponentProvider(false);
         provider.addIncludeFilter(new AssignableTypeFilter(Job.class));
         provider.addIncludeFilter(new AnnotationTypeFilter(GlassJob.class));
 
-        Iterable<String> basePackages = Splitter.on(',').omitEmptyStrings().trimResults().split(basePackage);
-        for (String bp : basePackages) {
-            for (BeanDefinition definition : provider.findCandidateComponents(bp)) {
-                JobAdder.createJobDetail(scheduler, definition.getBeanClassName());
+        try {
+            Iterable<String> basePackages = Splitter.on(',')
+                    .omitEmptyStrings().trimResults().split(basePackage);
+            for (String bp : basePackages) {
+                for (BeanDefinition definition : provider.findCandidateComponents(bp)) {
+                    JobAdder.createJobDetail(scheduler, definition.getBeanClassName());
+                }
             }
+        } catch (Exception ex) {
+            log.warn("scan jobs error", ex);
         }
     }
 
